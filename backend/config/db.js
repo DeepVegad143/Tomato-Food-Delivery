6762e2247d2d8dec3a96c4b9f46e91db20f651bd
Deep Vegad <23dcs142@charusat.edu.in>
2026-01-30 14:01:24 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://DeepVegad:143@cluster0.q8fenbd.mongodb.net/Tomato').then(()=>console.log('DB Conneted'));
+    await mongoose.connect('').then(()=>console.log('DB Conneted'));
 }
 
